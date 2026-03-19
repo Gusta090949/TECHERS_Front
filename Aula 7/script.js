37d8681ba58f35ee2e05bb706847c0ecd83006af
Gustavo Florencio de Caldas <gustavo.caldas>
2026-03-19 15:58:56 -0300

    Feat aula 7

---
+++ Aula 7/script.js
@@ -0,0 +1,10 @@
+
+const user ={
+    nome: "Ana",
+    sobrenome: "Conti",
+    idade: "24",
+    sayHello: function(){alert("Hello")}
+}
+alert(user.nome)
+console.log(user.idade)
+user.sayHello()
